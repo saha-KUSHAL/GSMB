ff8312f5e796380361f2f9394e950467d9f8ac6a
krbfx <krbfxofficial@gmail.com>
2022-12-03 23:57:28 -0800

    fixed compilation

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include "src/system.h"
-void main()
+int main()
 {
     c_printf("Gitaram Student Management BUS\n");
+    while(1)
+    
+    {
+        int ch;
+        printf("\tEnter 0 to exit.");
+        scanf("%d",&ch);
+        switch (ch){
+            case 0: exit(0);
+            break;
+            default: c_printf("\n\tEnter any inputs between 0 to ");
+        }
+        
+    }
+    return (0);
 }
--- src/system.c
+++ src/system.c
@@ -1,8 +1,8 @@
 #include "system.h"
 
 void c_printf(char s[])
-{
-    int length= sizeof(s)-1,width=80;
-    int pad = (length >= width) ? 0 : (width - length) / 2;
+{   
+    int width = 80;
+    int pad = (strlen(s) >= width) ? 0 : (width - strlen(s)) / 2;
     printf("%*.*s%s\n", pad, pad, " ", s);
 }
