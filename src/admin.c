2a9458eda525bf6b6b264706de22f8dbe4c1e014
krbfx <krbfxofficial@gmail.com>
2022-12-05 22:27:30 +0530

    admin panel work start

--- src/admin.c
+++ src/admin.c
@@ -5,10 +5,17 @@ void admin()
     if (login() == pass)
     {
         printf("\n");
+        red();
         c_printf("--ADMIN PANEL--");
+        green();
+        printf("\n--------------\t\t\t----------------");
+        printf("\n|(1)View Data|\t\t\t|(2)Manage Data|");
+        printf("\n--------------\t\t\t----------------");
+        printf("\n--------------\t\t\t---------");
+        printf("\n|(3))Settings|\t\t\t|(0)Exit|");
+        printf("\n--------------\t\t\t---------");
+        reset();
     }
     else
         printf("\nFail");
-
-    
 }
