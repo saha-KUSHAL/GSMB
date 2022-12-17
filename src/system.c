86b1136c5a081d7760a55d2f75e63d22754c796a
saha-KUSHAL <krbfxofficial@gmail.com>
2022-12-17 09:20:33 +0530

    modifed c_printf

--- src/system.c
+++ src/system.c
@@ -38,7 +38,7 @@ void reset()
 }
 void c_printf(char s[])
 {
-    int width = 80;
+    int width = 85;
     int pad = (strlen(s) >= width) ? 0 : (width - strlen(s)) / 2;
     printf("%*.*s%s\n", pad, pad, " ", s);
 }
